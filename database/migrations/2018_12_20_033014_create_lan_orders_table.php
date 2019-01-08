3dcfa4e85a7ddf4a0f3235039f622653b96c3c1d
zxcv1884 <zxcv1884@gmail.com>
2019-01-09 07:00:22 +0800

    2019-01-09finalv2

--- database/migrations/2018_12_20_033014_create_lan_orders_table.php
+++ database/migrations/2018_12_20_033014_create_lan_orders_table.php
@@ -16,7 +16,7 @@ class CreatelanOrdersTable extends Migration
         Schema::create('lan_orders', function (Blueprint $table) {
             $table->increments('id');
             $table->char('order_address',64)->collation('utf8_general_ci')->nullable();
-            $table->timestamp('order_at');
+            $table->timestamp('order_at')->nullable();
             $table->timestamp('order_finish_at')->nullable();
             $table->timestamps();
         });
